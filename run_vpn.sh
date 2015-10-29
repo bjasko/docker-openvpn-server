d8865731157e9a91274a193b10432db5e96db595
Jasmin Beganović <bjasko@bring.out.ba>
2015-10-29 10:23:52 +0100

    v2

--- run_vpn.sh
+++ run_vpn.sh
@@ -2,12 +2,12 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-VOLUME_BASE=$PWD 
+VOLUME_BASE=/data/openvpn 
 S_HOST=podrska
-S_DEV=wlan0
+S_DEV=eth0
 S_DOMAIN=bring.out.ba
-S_HOST_IP=${smtp_ip:-192.168.46.251}
-S_DNS_HOST_IP=${dns_lan_ip:-192.168.46.254}
+S_HOST_IP=${smtp_ip:-192.168.168.51}
+S_DNS_HOST_IP=${dns_lan_ip:-192.168.45.7}
 CT_NAME=podrska-vpn
 
 
@@ -16,4 +16,9 @@ sudo ip addr add $S_HOST_IP/24 dev $S_DEV
 
 
 docker rm -f  $CT_NAME
-docker run --privileged  --net=host -d  -v $VOLUME_BASE/etc/openvpn:/etc/openvpn --name $CT_NAME -p $S_HOST_IP:1194:1194/udp  openvpn-server
+docker run -d \
+      --privileged  --net=host -d \
+      -v $VOLUME_BASE/etc/openvpn:/etc/openvpn \
+      --name $CT_NAME \ 
+      -p $S_HOST_IP:1194:1194/udp  \
+      openvpn-server
